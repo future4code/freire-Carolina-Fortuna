34111898c87526a4c578311373f48519fcb09cf7
carolfortuna <63572811+carolfortuna@users.noreply.github.com>
2022-07-11 16:17:13 -0300

    Criação de paginas

---
+++ modulo2/labex/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      - `HomePage.js`  → Para o usuário escolher entre Área Administrativa e Lista de Viagens
+- `ListTripsPage.js` → Para vermos todas as viagens
+- `ApplicationFormPage.js` → Para o usuário se candidatar à viagens, página que vai ter o formulário de inscrição
+- `LoginPage.js` → Para fazermos login como administrador
+- `AdminHomePage.js` → Para o administrador ver a lista de viagens e poder deletá-las ou acessar o detalhe de cada uma delas
+- `TripDetailsPage.js` → Para o administrador ver o detalhe de uma viagem específica, bem como os candidatos que aplicaram para ela
+- `CreateTripPage.js`
+    </div>
+  );
+}
+
+export default App;
